2837eddfe2beff8dade0ae60a260562a234666d0
LoisClinton <loisclinton@hotmail.co.uk>
2023-12-13 17:15:59 +0000

    just implemented local storgae

--- src/react/App.tsx
+++ src/react/App.tsx
@@ -11,6 +11,20 @@ const UserContext = createContext();
 const App: React.FC = () => {
   const [currentUser, setCurrentUser] = useState(null);
 
+  // local storage stuff
+  useEffect(() => {
+    const storageItem = JSON.parse(localStorage.getItem("currentUserKey"));
+    if (storageItem) {
+      setCurrentUser(storageItem);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (currentUser != null) {
+      localStorage.setItem("currentUserKey", JSON.stringify(currentUser));
+    }
+  }, [currentUser]);
+
   return (
     <UserContext.Provider value={{ currentUser, setCurrentUser }}>
       <RouterProvider router={router} />;
