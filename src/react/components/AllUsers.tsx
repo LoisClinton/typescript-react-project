7497420c3fac69e1f2e3d45e4d4b5e135bd11d20
LoisClinton <loisclinton@hotmail.co.uk>
2023-12-13 15:15:09 +0000

    Web App can now display all users on the friends page

--- src/react/components/AllUsers.tsx
+++ src/react/components/AllUsers.tsx
@@ -10,11 +10,65 @@ const AllUsers: React.FC = ({
   addFriend,
   getFriends,
 }) => {
+  const userContext = useContext(UserContext);
+  const { currentUser, setCurrentUser } = userContext;
+  const [allUsers, setAllUsers] = useState();
+
+  const getUsers = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/api/users/`);
+
+      const data = await response.json();
+
+      console.log(data);
+
+      if (response.status != 200) {
+        throw new Error(data.message);
+      }
+
+      setAllUsers(data);
+
+      return;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
     <main>
-      <h1 className="text-yellow text-logo-large font-monomaniacone">
-        AllUsers Page
-      </h1>
+      <h1 className="text-yellow font-monomaniacone">ALL USERS:</h1>
+      <div className="landing-page-topics-container">
+        {allUsers
+          ? allUsers.map((user) => {
+              console.log(user);
+              if (user.email === currentUser.email) {
+                return;
+              } else {
+                return (
+                  <div className="quiz-topic-container background-light-grey">
+                    <h4 className="text-yellow font-opensans">
+                      {user.displayName}
+                    </h4>
+                    <p className="self-start text-yellow font-opensans">
+                      age: {user.age}
+                    </p>
+                    <p className="self-start text-yellow font-opensans">
+                      Favourite Topic: {user.favouriteTopic}
+                    </p>
+                    <p className=" self-start text-yellow font-opensans">
+                      Bio: {user.bio}
+                    </p>
+                    <button>ADD FRIEND</button>
+                  </div>
+                );
+              }
+            })
+          : "Loading.."}
+      </div>
     </main>
   );
 };
